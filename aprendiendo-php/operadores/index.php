01f36bf82837e441f2cc3bedd28447e3cded9995
Pulpo-julian <julian.schrodinger@gmail.com>
2022-08-21 12:51:06 -0500

    operadores aritmeticos

---
+++ aprendiendo-php/operadores/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $numero1 = 82;
+    $numero2 = 33;
+
+    echo 'la Suma da: '.($numero1 + $numero2).'<br/>';
+    echo 'la resta da: '.($numero1 - $numero2).'<br/>';
+
+    echo 'la division da: '.($numero1 / $numero2).'<br/>';
+    
+
+?>
