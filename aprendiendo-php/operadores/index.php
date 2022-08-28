f710ef1e9457fd13e22f86611a9d8e41e11954dc
Pulpo-julian <julian.schrodinger@gmail.com>
2022-08-28 12:40:32 -0500

    superglobales

--- aprendiendo-php/operadores/index.php
+++ aprendiendo-php/operadores/index.php
@@ -1,12 +1,19 @@
 <?php
 
-    $numero1 = 82;
+    $numero1 = 2;
     $numero2 = 33;
 
     echo 'la Suma da: '.($numero1 + $numero2).'<br/>';
     echo 'la resta da: '.($numero1 - $numero2).'<br/>';
 
     echo 'la division da: '.($numero1 / $numero2).'<br/>';
+    echo 'la multiplicacion da: '.($numero1 * $numero2).'<br/>';
+
+    //Operadores de incremento y decremento
+    $year = 2019;
+
+    echo "<h1>".($year++)."<h1/>";
+    echo "<h1>".($year)."<h1/>";
     
 
 ?>
